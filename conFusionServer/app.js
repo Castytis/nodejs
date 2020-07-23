c9a5317700aed8cd0d34af58a7ac0b626f6fa426
kasty <kastytis.slubelis@gmail.com>
2020-07-23 08:35:43 +0300

    Passports

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -10,6 +10,9 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 //autorizacijai
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
@@ -18,7 +21,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -41,28 +44,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 // before con
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-if(!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
-}
-else {
-  if (req.session.user === 'authenticated') {
-    next();
+    next(err);
   }
   else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
+        next();
   }
-}
 }
 
 app.use(auth);

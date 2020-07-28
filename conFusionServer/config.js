d8047b47322678c72bfdf82e356b0053448ddb01
kasty <kastytis.slubelis@gmail.com>
2020-07-28 22:55:09 +0300

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '777362816373079',
+        clientSecret: '824182e7f34dd9c3e46fcd4b7082354c'
+    }
 }
